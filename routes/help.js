005dc47d84713d0447bd3b69e7d348c62d2f473e
Rowan Manning <rowan.manning@nature.com>
2015-07-30 08:58:45 +0100

    Improve the help page

--- routes/help.js
+++ routes/help.js
@@ -5,7 +5,17 @@ module.exports = initRoute;
 function initRoute (app, robot) {
 	app.get('/help', function (request, response) {
 		response.render('help', {
-			commands: robot.helpCommands()
+			commands: robot.helpCommands().map(processHelpCommand.bind(null, robot))
 		});
 	});
+}
+
+function processHelpCommand (robot, rawCommand) {
+	var parts = rawCommand.replace(/hubot/ig, robot.name).split('-');
+	var name = parts.shift().trim();
+	var description = parts.join('-').trim();
+	return  {
+		name: name,
+		description: description
+	};
 }
